9f914d4a4c78648ec281b9cf54866009da53c147
e <tubeikai@163.com>
2019-10-20 19:18:29 +0800

    增加hello world

--- src/main/java/Demo01.java
+++ src/main/java/Demo01.java
@@ -2,5 +2,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello java!!");
         System.out.println("hello world!!");
+        System.out.println("hello");
     }
 }
